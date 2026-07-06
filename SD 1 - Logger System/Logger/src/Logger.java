eaba76f871365d98c9c346caa80742b9d20dd610
Nishaanth Vikram <nishaanth2696@gmail.com>
2026-07-06 23:01:19 +0530

    In a single file java machine code

--- SD 1 - Logger System/Logger/src/Logger.java
+++ SD 1 - Logger System/Logger/src/Logger.java
@@ -1,57 +1,160 @@
-import java.security.Timestamp;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Logger {
-    private static Logger instance;
-    private LogLevel level;
-    private List<Appender> appenders;
+enum LogLevel{
+    DEBUG,
+    INFO,
+    WARN,
+    ERROR
+}
 
-    private Logger() {
+class LogMessage {
+    private String message;
+    private LogLevel level;
+    private long timestamp;
+
+    public LogMessage(String message, LogLevel level) {
+        this.message = message;
         this.level = level;
-        this.appenders = new ArrayList<>();
+        this.timestamp = Instant.now().toEpochMilli();
     }
 
-    public static synchronized Logger getInstance() {
+//    getter
+
+    public String getMessage() {
+        return message;
+    }
+    public LogLevel getLevel() {
+        return level;
+    }
+    public long getTimestamp() {
+        return timestamp;
+    }
+}
+
+interface LogAppender {
+    void append(LogMessage logMessage);
+}
+
+class ConsoleAppender implements LogAppender {
+    private String formatter;
+//    constructor
+    public ConsoleAppender(String formatter) {
+        this.formatter = formatter;
+    }
+    @Override
+    public void append(LogMessage logMessage) {
+        System.out.println(logMessage.toString());
+    }
+}
+
+class FileAppender implements LogAppender {
+    private String formatter;
+    public FileAppender(String formatter) {
+        this.formatter = formatter;
+    }
+    @Override
+    public void append(LogMessage logMessage) {
+        System.out.println(logMessage.toString());
+    }
+}
+
+class HandleLogHandler {
+    private List<LogAppender> observers = new ArrayList<>();
+
+    public void subscribe(LogAppender observer) {
+        observers.add(observer);
+    }
+
+    public void notifyObservers(LogMessage logMessage) {
+        for (LogAppender observer : observers) {
+            if (canHandle(logMessage.getLevel(), logMessage)) {
+                observer.append(logMessage);
+            }
+        }
+    }
+
+    private boolean canHandle(LogLevel level, LogMessage logMessage) {
+        return true;
+    }
+
+    public void handleMsg(LogMessage logMessage) {
+        notifyObservers(logMessage);
+    }
+}
+
+class DebugNotifier {
+    public boolean canHandle(LogLevel level, LogMessage logMessage) {
+        return level == LogLevel.DEBUG;
+    }
+}
+
+class InfoNotifier {
+    public boolean canHandle(LogLevel level, LogMessage logMessage) {
+        return level == LogLevel.INFO;
+    }
+}
+
+class WarnNotifier {
+    public boolean canHandle(LogLevel level, LogMessage logMessage) {
+        return level == LogLevel.WARN;
+    }
+}
+
+
+class ErrorNotifier {
+    public boolean canHandle(LogLevel level, LogMessage logMessage) {
+        return level == LogLevel.ERROR;
+    }
+}
+
+public class Logger {
+    private static Logger instance;
+    private HandleLogHandler handleLogHandler;
+
+    private Logger() {
+        handleLogHandler = new HandleLogHandler();
+        handleLogHandler.subscribe(new ConsoleAppender("Console"));
+        handleLogHandler.subscribe(new FileAppender("File"));
+    }
+
+    public static Logger getInstance() {
         if (instance == null) {
             instance = new Logger();
         }
         return instance;
     }
 
-    public void addAppender(Appender appender) {
-        this.appenders.add(appender);
-    }
-
-    public void setLevel(LogLevel level) {
-        this.level = level;
-    }
-
-    private void log(LogLevel level, String message) {
-        if (level.ordinal() < this.level.ordinal()) {
-            return;
-        }
-
-        LogMessage logMessage = new LogMessage(message, level);
-
-        for (Appender appender : this.appenders) {
-            appender.append(logMessage);
-        }
+    public void handleMsg(LogMessage logMessage) {
+        handleLogHandler.handleMsg(logMessage);
     }
 
     public void info(String message) {
-        this.log(LogLevel.INFO, message);
+        LogMessage logMessage = new LogMessage(message, LogLevel.INFO);
+        handleMsg(logMessage);
     }
 
     public void warn(String message) {
-        this.log(LogLevel.WARN, message);
+        LogMessage logMessage = new LogMessage(message, LogLevel.WARN);
+        handleMsg(logMessage);
     }
 
     public void error(String message) {
-        this.log(LogLevel.ERROR, message);
+        LogMessage logMessage = new LogMessage(message, LogLevel.ERROR);
+        handleMsg(logMessage);
     }
 
     public void debug(String message) {
-        this.log(LogLevel.DEBUG, message);
+        LogMessage logMessage = new LogMessage(message, LogLevel.DEBUG);
+        handleMsg(logMessage);
+    }
+
+    static void main() {
+        Logger logger = getInstance();
+        logger.debug("this is a debug message");
+        logger.info("this is a info message");
+        logger.warn("this is a warn message");
+        logger.error("this is a error message");
     }
 }
